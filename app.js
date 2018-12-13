55e2dd3bd20e3c8350a16ffab2d62c616770d523
shawnmartin <martin.sh@husky.neu.edu>
2018-12-13 12:09:15 -0500

    changed start script

--- app.js
+++ app.js
@@ -5,16 +5,18 @@ const bodyParser = require('body-parser');
 
 const api = require('./src/routes/api');
 
+let local_uri = 'mongodb://localhost/extracredit';
+let heroku_uri = 'mongodb://heroku_5lxsd1jq:pla3gi3m3q1k802i95kquta9sq@ds135382.mlab.com:35382/heroku_5lxsd1jq';
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-const connect = function (database_name) {
-    return mongoose.connect('mongodb://localhost/' + database_name, { useNewUrlParser: true });
+const connect = function (uri) {
+    return mongoose.connect(uri, { useNewUrlParser: true });
 };
 
-let dbname = 'extracredit';
 
-connect(dbname)
+connect(heroku_uri)
 .then(() => {
     app.use('/api', api);
 
